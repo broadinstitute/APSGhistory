0cf9a055bb83e4efb6e46a79a1bad65c671204be
ckd <ckd@95e82132-2061-11dd-9cc4-33af95911ecc>
2008-09-24 14:42:39 +0000

    # on 2007/03/20 18:43:32, ckd did: fix logic to only update state for "newer log file" when the run was previously marked complete; we don't want to stomp on 'syncing' runs.
    git-svn-id: https://subversion01.broadinstitute.org/APSG@159 95e82132-2061-11dd-9cc4-33af95911ecc

--- fromRCS/db-solexa-rsync.py
+++ fromRCS/db-solexa-rsync.py
@@ -132,14 +132,16 @@ def update_run_info(deck,rundir,logfile,log_change):
     if lastlogdate < log_change:
         curs.execute("""MERGE INTO runs r USING dual ON (r.run_name = :rname)
         WHEN matched THEN
-        UPDATE SET log_last_changed = :mtime,
-                   log_currfile = :logcurr, state = :state
+        UPDATE SET log_last_changed = :mtime, log_currfile = :logcurr
         WHEN NOT matched THEN
         INSERT (run_name, deck_name, run_sourcepath, log_currfile,
                 log_last_changed, state)
         VALUES (:rname,:dname,:rpath,:logcurr, :mtime,:state)""",
                      rname=run_name,dname=deck,rpath=rundir, logcurr=logfile,
                      mtime=log_change,state='pending')
+        curs.execute("""UPDATE runs SET state = 'pending'
+                        WHERE runs.run_name = :rname AND state = 'complete'""",
+                     rname=run_name);
 
 def check_deck(deck,basedir):
     logpath = os.path.join(basedir,logdir)
