ba0a269620b0776ca80fab3e05da856e3a7f86e7
ckd <ckd@95e82132-2061-11dd-9cc4-33af95911ecc>
2008-09-24 14:41:31 +0000

    # on 2006/12/08 22:29:43, ckd did: fix typo in bind vars
    git-svn-id: https://subversion01.broadinstitute.org/APSG@101 95e82132-2061-11dd-9cc4-33af95911ecc

--- fromRCS/db-solexa-rsync.py
+++ fromRCS/db-solexa-rsync.py
@@ -106,7 +106,7 @@ def update_run_info(deck,rundir,logpath,log_mtime):
     if not lastlogdate or lastlogdate > log_change:
         curs.execute("""MERGE INTO runs r USING dual ON (r.run_name = :rname)
         WHEN matched THEN
-        UPDATE SET log_last_changed = :mtime, log_currfile = :currfile
+        UPDATE SET log_last_changed = :mtime, log_currfile = :logcurr
         WHEN NOT matched THEN
         INSERT (run_name, deck_name, run_sourcepath, log_currfile,
                 log_last_changed, state)
