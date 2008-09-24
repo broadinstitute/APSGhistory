abe2b6488140d34d7675e08c35af415aff9900bc
apsg <apsg@95e82132-2061-11dd-9cc4-33af95911ecc>
2008-09-24 14:40:29 +0000

    # on 2006/10/27 14:53:39, apsg did: optparse added for cmd line flags
    git-svn-id: https://subversion01.broadinstitute.org/APSG@58 95e82132-2061-11dd-9cc4-33af95911ecc

--- fromRCS/dircleaner.py
+++ fromRCS/dircleaner.py
@@ -1,20 +1,12 @@
 #!/util/bin/python
 
 import os,stat,time
+from optparse import OptionParser
 
 # allow an hour for files even with no procs running
 # to avoid race conditions or the like
 window = 60 * 60
 exptime = int(time.time() - window)
-
-proclist = os.popen('ps --no-headers -eo uid')
-
-hasproc = {}
-
-for line in proclist:
-    hasproc[int(line)] = True
-
-proclist.close()
 
 def handle_item(dir,file):
     path = os.path.join(dir,file)
@@ -31,14 +23,58 @@ def handle_item(dir,file):
         # sockets, devices, whatever: ignore them
         return
     if timestamp < exptime:
+        status = ''
         if stat.S_ISDIR(mode):
-            # try: os.rmdir, except OSError: return
-            print 'rmdir:',path
+            if not options.dry_run:
+                try:
+                    print 'os.rmdir',path
+                except OSError:
+                    status = 'failed '
+            if options.verbose:
+                print status + 'rmdir:',path
         else:
-            print 'rm:',path
+            if not options.dry_run:
+                try:
+                    print 'os.remove',path
+                except OSError:
+                    status = 'failed '
+            if options.verbose:
+                print status + 'rm:',path
 
-for (dirname, dirshere, fileshere) in os.walk('/tmp', topdown=False):
-    for file in fileshere:
-        handle_item(dirname,file)
-    for dir in dirshere:
-        handle_item(dirname,dir)
+def main():
+    global options, hasproc
+
+    usage = 'usage: %prog [options] directory ...'
+    parser = OptionParser(usage)
+    parser.add_option('-f', '--force', action='store_true', dest='force',
+                      help='force removal in non scratch/tmp directories')
+    parser.add_option('-n', '--dry-run', action='store_true', dest='dry_run',
+                      help='do not remove files or directories. implies -v')
+    parser.add_option('-v', '--verbose', action='store_true', dest='verbose',
+                      help='show files and directories being removed')
+    (options,args) = parser.parse_args()
+    if len(args) == 0:
+        parser.error('No directories given!')
+
+    hasproc = {}
+    proclist = os.popen('ps --no-headers -eo uid')
+    for line in proclist:
+        hasproc[int(line)] = True
+    proclist.close()
+
+    if options.dry_run:
+        options.verbose = True
+    for tree in args:
+        if options.force or '/tmp' in tree or '/scratch' in tree:
+            if options.verbose:
+                print 'processing',tree
+            for (dirname, dirshere, fileshere) in os.walk(tree, topdown=False):
+                for file in fileshere:
+                    handle_item(dirname,file)
+                for dir in dirshere:
+                    handle_item(dirname,dir)
+        else:
+            print 'skipping %s, use -f to force' % tree
+
+if __name__ == '__main__':
+    main()
