d1cef6ee1219242f52571d9f365a3e005b0bb7b5
apsg <apsg@95e82132-2061-11dd-9cc4-33af95911ecc>
2008-09-24 14:44:29 +0000

    # on 2008/02/13 17:43:29, apsg did: mod unpack_blastdb.py for temp isilon directory
    git-svn-id: https://subversion01.broadinstitute.org/APSG@259 95e82132-2061-11dd-9cc4-33af95911ecc

---
+++ fromRCS/unpack_blastdb_isilon.py
@@ -0,0 +1,30 @@
+#!/util/bin/python
+
+import os,tarfile
+
+downloads_dir = '/broad/data/blastdb-download'
+unpack_dir = '/broad/data/blastdb'
+semaphore_file = os.path.join(unpack_dir,'.unpack_done')
+
+os.spawnl(os.P_WAIT,'/bin/rm','rm','-rf',unpack_dir);
+
+tarfiles = sorted(os.listdir(downloads_dir))
+
+for file in tarfiles:
+    dbname = file.split('.')[0]
+    tarpath = os.path.join(downloads_dir,file)
+    dstpath = os.path.join(unpack_dir,dbname)
+    if not os.path.exists(dstpath):
+        os.makedirs(dstpath,0755)
+    print tarpath
+    tar = tarfile.open(tarpath)
+    # throw an exception for *any* problems
+    tar.errorlevel = 2
+    # XXX python 2.5 would use: tar.extractall(dstpath)
+    for member in tar.getmembers():
+        tar.extract(member,dstpath)
+    tar.close()
+
+sem = open(semaphore_file,'w')
+sem.write('Unpack completed\n')
+sem.close()
