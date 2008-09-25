0c033c3b13e3891f45b431b5a92d8c40594e2241
adb <adb@95e82132-2061-11dd-9cc4-33af95911ecc>
2008-09-25 06:20:15 +0000

    database-less expiry script
    git-svn-id: https://subversion01.broadinstitute.org/APSG@291 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/pypeline.py
+++ scripts/pypeline.py
@@ -1,56 +1,87 @@
 #!/usr/bin/env python
 
 from glob import glob
-from re   import search
-from os   import stat
 from time import time
+
+import os, re
 
 class SolexaRun:
 	"data and images from a Solexa sequencer run"
 
 	import cx_Oracle
+	
+	ocs_file = '/broad/tools/etc/seq20-connectstring'
+ 	oracle   = cx_Oracle.connect(open(ocs_file).readline().strip())
 
-	oracle = cx_Oracle.connect('slxasync/c0piiRn2pr@seqprod')
-
-	def __init__(self, name,
-	             deck = None, barcode = None,
-	             images = None, data = None):
+	def __init__(self, name, images = None, data = None):
 
 		self.name    = name
+		parsed_name  = re.search('^(.*?)_(.*?)_(.*?)_(.*)$', name)
+		if not parsed_name: raise name
+		deck         = parsed_name.group(2)
 
-		parsed_name  = search('^(.*?)_(.*?)_(.*?)_(.*)$', name)
-
-		self.deck    = deck    or parsed_name.group(2)
-		self.barcode = barcode or parsed_name.group(4)
-	
-		def infer_images(deck, name):
+		self.images = images
+		if not images:
 			dirs = glob("/slxa/%s_images/transfer/mirror/%s" % (deck, name))
-
-			if dirs: return dirs[0]
-			else:    return None
+			if dirs: self.images = dirs[0]
+			else:    raise 'images directory not found for run %s' % name
 			
-		def infer_data  (deck, name):
-			dirs = glob("/seq/solexaproc/%s/analyzed/%s"     % (deck, name))
-			
-			if dirs: return dirs[0]
-			else:    return None
+		recipes = glob('%s/Recipe*' % self.images)
+		if not recipes:      raise "recipe not found for %s"       % name
+		if len(recipes) > 1: raise "multiple recipes found for %s" % name
+		
+		read_type, no_cycles = None, None
+		
+		recipe = open(recipes[0])
+		for line in recipe:
+			rtm = re.search("<!--\s+Read Type: (.*)\s+-->", line)
+			ncm = re.search("<!--\s+No\. Cycles: (\d+)\s+-->", line)
+			if rtm: read_type = rtm.group(1)
+			if ncm: no_cycles = int(ncm.group(1))
+		recipe.close
+		
+		self.cycles = no_cycles
+		self.paired = (read_type == "Paired End")
 
-		self.data    = data    or infer_data  (self.deck, name)
-		self.images  = images  or infer_images(self.deck, name)
+	def analysis_pending(self):
+		"analysis directories that we care about that are not finished"
+
+		def relevant(name):
+			match = re.search('^C1-(\d+)_Firecrest.*_prodinfo$',
+			 				  os.path.split(name)[-1])
+			if not match: return False
+
+			cycles = int(match.group(1))
+
+			if (cycles == self.cycles):                       return True
+			if (cycles == self.cycles * 2) and (self.paired): return True
+		
+		def pending(name):
+			return not os.path.isfile("%s/finished.txt" % name)
+			
+		analyses = glob("%s/Data/C*_prodinfo" % self.images)
+		
+		return [ os.path.split(path)[-1]
+				 for path in filter(relevant, filter(pending, analyses)) ]
 		
 	def image_status(self):
 		"Can the images from the run be marked as expired?"
 
+		if not self.images: return 'missing'
+
+		pending = self.analysis_pending()
+
 		LIFETIME = 60 * 60 * 24 * 14
 
-		if not self.images: return 'missing'
-		
-		if stat(self.images).st_mtime + LIFETIME > time(): return 'fresh'
-
-		return self.__dict__
+		if pending: return 'waiting for analysis in %s' % ' '.join(pending)
+		elif os.stat(self.images).st_mtime + LIFETIME > time(): return 'fresh'
+		else: return 'expired'
 
 if __name__ == "__main__":
 
 	from sys import argv
 
-	print [ SolexaRun(run_id).image_status() for run_id in argv[1:] ]
+	runs = [ SolexaRun(run_id) for run_id in argv[1:] ]
+
+	for run in runs:
+		print "%s: %s" % (run.images, run.image_status())
