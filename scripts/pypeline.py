b09b5ecc3e08ecbeffab25c56a7d73c60d0a7d62
adb <adb@95e82132-2061-11dd-9cc4-33af95911ecc>
2008-09-25 03:12:30 +0000

    finds directories and reports whether images are missing or fresh
    git-svn-id: https://subversion01.broadinstitute.org/APSG@290 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/pypeline.py
+++ scripts/pypeline.py
@@ -2,31 +2,55 @@
 
 from glob import glob
 from re   import search
+from os   import stat
+from time import time
 
 class SolexaRun:
 	"data and images from a Solexa sequencer run"
 
-	def __init__(self, name, deck = None, images = None, data = None):
-	
-		def infer_deck(name):
-			return search('^(.*?)_(.*?)_(.*?)_(.*)$', name).group(2)
+	import cx_Oracle
+
+	oracle = cx_Oracle.connect('slxasync/c0piiRn2pr@seqprod')
+
+	def __init__(self, name,
+	             deck = None, barcode = None,
+	             images = None, data = None):
+
+		self.name    = name
+
+		parsed_name  = search('^(.*?)_(.*?)_(.*?)_(.*)$', name)
+
+		self.deck    = deck    or parsed_name.group(2)
+		self.barcode = barcode or parsed_name.group(4)
 	
 		def infer_images(deck, name):
-			return glob("/slxa/%s_images/transfer/mirror/%s" % (deck, name))
+			dirs = glob("/slxa/%s_images/transfer/mirror/%s" % (deck, name))
+
+			if dirs: return dirs[0]
+			else:    return None
 			
-		def infer_data(deck, name):
-			return glob("/seq/solexaproc/%s/analyzed/%s"     % (deck, name))
+		def infer_data  (deck, name):
+			dirs = glob("/seq/solexaproc/%s/analyzed/%s"     % (deck, name))
 			
-		self.name   = name
-		self.deck   = deck   or infer_deck(name)
-		self.data   = data   or infer_data(deck, name)
-		self.images = images or infer_images(deck, name)
+			if dirs: return dirs[0]
+			else:    return None
+
+		self.data    = data    or infer_data  (self.deck, name)
+		self.images  = images  or infer_images(self.deck, name)
 		
-	def status(self):
+	def image_status(self):
+		"Can the images from the run be marked as expired?"
+
+		LIFETIME = 60 * 60 * 24 * 14
+
+		if not self.images: return 'missing'
+		
+		if stat(self.images).st_mtime + LIFETIME > time(): return 'fresh'
+
 		return self.__dict__
 
 if __name__ == "__main__":
 
 	from sys import argv
 
-	print [ SolexaRun(run_id).status() for run_id in argv[1:] ]
+	print [ SolexaRun(run_id).image_status() for run_id in argv[1:] ]
