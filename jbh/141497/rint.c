787efe8816e2f1340abf30da64caea1cbc0df6ef
jbh <jbh@95e82132-2061-11dd-9cc4-33af95911ecc>
2009-01-07 18:19:00 +0000

    Added directories to store centos config foo.
    git-svn-id: https://subversion01.broadinstitute.org/APSG@298 95e82132-2061-11dd-9cc4-33af95911ecc

--- jbh/141497/rint.c
+++ jbh/141497/rint.c
@@ -1,12 +1,67 @@
 #include <stdio.h>
-
 #include <math.h>
 
 int main(int argc, char **argv)
 {
-  double a, b ;
-  double rint(double x) ;
+  double a, b;
+  double rint(double x);
 
-  a = 289440.59 ; b = rint(a) ; printf("%15.12f %15.12f\n", a, b) ;
-  a = 289440.75 ; b = rint(a) ; printf("%15.12f %15.12f\n", a, b) ;
+  a = 0.59; 
+  b = rint(a); 
+  printf("%15.12f %15.12f\n", a, b);
+  
+  a = 0.75; 
+  b = rint(a); 
+  printf("%15.12f %15.12f\n", a, b);
+  
+  a = 1.59; 
+  b = rint(a); 
+  printf("%15.12f %15.12f\n", a, b);
+  
+  a = 1.74999999; 
+  b = rint(a); 
+  printf("%15.12f %15.12f\n", a, b);
+
+  a = 1.75; 
+  b = rint(a); 
+  printf("%15.12f %15.12f\n", a, b);
+
+  a = 2.59; 
+  b = rint(a); 
+  printf("%15.12f %15.12f\n", a, b);
+  
+  a = 2.75; 
+  b = rint(a); 
+  printf("%15.12f %15.12f\n", a, b);
+
+  a = 289440.59; 
+  b = rint(a); 
+  printf("%15.12f %15.12f\n", a, b);
+  
+  a = 289440.75; 
+  b = rint(a); 
+  printf("%15.12f %15.12f\n", a, b);
+  
+  a = 289441.59; 
+  b = rint(a); 
+  printf("%15.12f %15.12f\n", a, b);
+  
+  a = 289441.74999999; 
+  b = rint(a); 
+  printf("%15.12f %15.12f\n", a, b);
+
+  a = 289441.75; 
+  b = rint(a); 
+  printf("%15.12f %15.12f\n", a, b);
+
+  a = 289442.59; 
+  b = rint(a); 
+  printf("%15.12f %15.12f\n", a, b);
+  
+  a = 289442.75; 
+  b = rint(a); 
+  printf("%15.12f %15.12f\n", a, b);
+  
+
+  
 }
