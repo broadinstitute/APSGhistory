ee8ea5a1758f6815de347c1a1bf1c1ed12746332
jlchang <jlchang@95e82132-2061-11dd-9cc4-33af95911ecc>
2009-01-28 16:33:58 +0000

    updated thumper setup script -jlc
    git-svn-id: https://subversion01.broadinstitute.org/APSG@300 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/setup_thumper_zfs.sh
+++ scripts/setup_thumper_zfs.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 zfs create zpool1/$1
 zfs create zpool1/$1_images
-zfs set compress=on zpool1/$1
-zfs set reserv=5T zpool1/$1 zpool1/$1_images
-zfs set quota=7T zpool1/$1 zpool1/$1_images
+zfs set compress=on zpool1/$1 zpool1/$1_images
+zfs set quota=500G zpool1/$1 
+zfs set quota=6T zpool1/$1_images
 mkdir /zpool1/$1_images/transfer /zpool1/$1/analyzed
 chmod 775 /zpool1/$1_images/transfer /zpool1/$1/analyzed
-chown -R prodinfo:seqswprod /zpool1/$1 /zpool1/$1_images
+chown -R prodinfo:sequence /zpool1/$1 /zpool1/$1_images
 ln -s /slxa/$1_images/transfer /zpool1/$1
 ln -s /seq/solexaproc/solexaproc01/Instruments /zpool1/$1
 zfs set sharenfs=rw=@18.103.8.0/22:@18.103.32.0/20,root=@18.103.8.0/22:@18.103.32.0/20 zpool1/$1
