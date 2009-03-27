e2603698a4f25b385c3be79bb7a1ea699e1a4851
adb <adb@95e82132-2061-11dd-9cc4-33af95911ecc>
2009-03-27 13:19:51 +0000

    correct regexp for profiles.xml/pom.xml reports to releng
    git-svn-id: https://subversion01.broadinstitute.org/APSG@401 95e82132-2061-11dd-9cc4-33af95911ecc

--- subversion/seq/hooks/post-commit.d/00-summary-email.rb
+++ subversion/seq/hooks/post-commit.d/00-summary-email.rb
@@ -7,4 +7,4 @@
 # (per RT #147945) summarize commits of files controlling inclusion of
 # third-party objects
 
-/broad/tools/scripts/svn-summary-email.rb "$@" '^/main/sequence/.*/(profiles|pom).xml$' releng@broad.mit.edu
+/broad/tools/scripts/svn-summary-email.rb "$@" '^main/sequence/.*/(profiles|pom).xml$' releng@broad.mit.edu
