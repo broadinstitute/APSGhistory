e46941852633daa8e3af3dd55eb89d74976b4a31
adb <adb@95e82132-2061-11dd-9cc4-33af95911ecc>
2009-03-27 13:11:22 +0000

    commit some changes made a couple months back
    git-svn-id: https://subversion01.broadinstitute.org/APSG@399 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/archive-miscellaneous-files.sh
+++ scripts/archive-miscellaneous-files.sh
@@ -7,12 +7,13 @@ set -e
 
 cd /imaging/dropbox
 
-find . -mindepth 1 -maxdepth 1 -name 'ToBeDeleted.*' -print0 |
-	/util/bin/ruby -rtime -0 -ne 'print if $_ =~ %r{^\./(ToBeDeleted\.(\d+-\d+-\d+))\0$} and Time.parse($2) <= Time.parse("today")' |
-		xargs -0 rm -rf
+find ToBeDeleted -mindepth 1 -maxdepth 1 -print0 |
+	/util/bin/ruby -rtime -0 -ne 'print if $_ =~ %r{^ToBeDeleted/(\d+-\d+-\d+)\0$} and Time.parse($1) <= Time.parse("today")' |
+		xargs -0t --no-run-if-empty rm -rf
 
 DELETION_DATE=`date -I --date "today +$DELETE_DAYS days"`
-ARCHIVE_DIR=ToBeDeleted.$DELETION_DATE
+ARCHIVE_DIR=ToBeDeleted/$DELETION_DATE
 
 mkdir -p $ARCHIVE_DIR
-find . -mindepth 1 -maxdepth 1 -mtime +$ARCHIVE_DAYS -print0 | xargs -0i mv {} $ARCHIVE_DIR
+find . -mindepth 1 -maxdepth 1 \( -not -name ToBeDeleted \) -mtime +$ARCHIVE_DAYS -print0 |
+    xargs -0t -I {} --no-run-if-empty mv {} $ARCHIVE_DIR
