7a598cd8b4df9fb2e21ad25817998cf105c65ae7
jbh <jbh@95e82132-2061-11dd-9cc4-33af95911ecc>
2009-04-16 15:42:09 +0000

    Added first draft of checkNFS.sh, script to test for hung NFS mounts.
    git-svn-id: https://subversion01.broadinstitute.org/APSG@416 95e82132-2061-11dd-9cc4-33af95911ecc

--- local/setstartup.sh
+++ local/setstartup.sh
@@ -25,12 +25,19 @@ done
 # Display a menu
 echo "Select an option:"
 echo "  1 - Use classic startup scripts."
-echo "  2 - Use dotkit startup scripts."
+echo
+echo "  2 - Use dotkit startup scripts. You may want"
+echo "      to modify your .my.cshrc and/or .my.bash*" 
+echo "      files to use dotkit functions."
+echo
+echo "  3 - Use dotkit-dev startup scripts. "
+echo "      WARNING: This is development and is "
+echo "               subject to rapid changes."
 echo "  q - Quit."
 echo 
 
 # Collect response
-read -p " (1,2,q): " RESPONSE
+read -p " (1,2,3,q): " RESPONSE
 
 case "$RESPONSE" in
   "1" )
@@ -39,6 +46,8 @@ case "$RESPONSE" in
   "2" )
     install dotkit
     ;;
+  "3" )
+    install dotkit-dev
   * ) 
     echo "No change made."
     ;;
