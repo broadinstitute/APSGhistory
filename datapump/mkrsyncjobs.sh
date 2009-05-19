bf2ced19848206d2cc7bc1253d442284518fc27b
jbh <jbh@95e82132-2061-11dd-9cc4-33af95911ecc>
2009-05-19 14:04:09 +0000

    Made delete-behind scripts more generic, less SOLiD-y.
    git-svn-id: https://subversion01.broadinstitute.org/APSG@486 95e82132-2061-11dd-9cc4-33af95911ecc

--- datapump/mkrsyncjobs.sh
+++ datapump/mkrsyncjobs.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+
+
 for run in $(<$1)
   do
-    SOLID=`echo $run | cut -f1 -d"|"`
+    INSTRUMENT=`echo $run | cut -f1 -d"|"`
     RUN=`echo $run | cut -f2 -d"|"`
-
+    SRCROOT=`echo $run | cut -f3 -d"|"`
+    DSTROOT=`echo $run | cut -f4 -d"|"`
+    
     cat rsync.template | \
-        sed -s "s/%SOLID%/${SOLID}/" | \
+        sed -s "s/%INSTRUMENT%/${INSTRUMENT}/" | \
         sed -s "s/%RUN%/${RUN}/" | \
-        sed -s "s/%JOBNAME%/${SOLID}-${RUN}/" > \
-        RSYNC-${SOLID}-${RUN}.job
+        sed -s "s/%JOBNAME%/${INSTRUMENT}-${RUN}/" |
+        sed -s "s/%SRCROOT%/${SRCROOT}/" | 
+        sed -s "s/%DSTROOT%/${DSTROOT}/" > \
+        RSYNC-${INSTRUMENT}-${RUN}.job
   done
