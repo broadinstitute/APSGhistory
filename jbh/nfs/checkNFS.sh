b6375368c3b981ba0d06c07cc3a23495fa9d3e4a
jbh <jbh@95e82132-2061-11dd-9cc4-33af95911ecc>
2009-05-22 19:51:32 +0000

    Moved work to functions. Added sample ypcat output to use for offline testing.
    git-svn-id: https://subversion01.broadinstitute.org/APSG@503 95e82132-2061-11dd-9cc4-33af95911ecc

--- jbh/nfs/checkNFS.sh
+++ jbh/nfs/checkNFS.sh
@@ -7,16 +7,22 @@
 # jbh - 2009-04-16
 #
 
+# verbose
+# Echo argument if VERBOSE is defined.
+function verbose () {
+  if [ ! -z "$VERBOSE" ]; then
+    echo $1 >&2
+  fi
+}
 
 # Recursive function to find the NFS mountpoint 
-function findNfsMount () {
-
+function isNfsMount () {
   # check if arg is an nfs mountpoint
   MPINFO=`mount | grep $1 | grep "type nfs"`
   MPRESULT=$?
   if [ $MPRESULT == 0 ]; then 
     NFS=`echo $MPINFO | awk '{print $1}'`
-    echo "$1 is mounted from $NFS"
+    verbose "Mounted at $NFS"
     return 0
   fi
 
@@ -24,51 +30,80 @@ function findNfsMount () {
   MP=`dirname $1`
   if [ "x$MP" == "x/" ]; then
     # If we are all the way to /, exit with error. 
+    verbose "Hit bottom at $MP."
     return 1
   else
     # Check next level down.
-    findNfsMount ${MP}
+    isNfsMount ${MP}
     return $?
   fi
 }
 
+function isAutofsMount () {
+ # This function shoudl parse the ypcat output to verify this is a broad automount.
+ return 1
+}
 
+function isAccessible () {
+  # Verify that this is not a hung NFS mount (or hung mount 
+  # of any kind, i guess. Use a 10 second timeout. That should 
+  # be plenty for an automount to take place if necessary and 
+  # return something. The something should be the number 1
+  # even if the path is non-existant.i
+  unset RESULT
+  read -t10 RESULT < <( ls -ld $1 2>&1 | wc -l )
+
+  # Collect the result of the read. 
+  # 0 means it was successful, that is, it did not block and/or timeout.
+  # 1 means it timed out.
+  READ=$?
+
+  return $READ
+}
+
+
+######################################################################
 # Main Program
 
-# Make sure we have an argument to check.
+# Process arguments
+while getopts "nv" OPTION; do
+  case $OPTION in 
+    n ) NOTIFY=true ;;
+    v ) VERBOSE=true ;;
+    * ) 
+      echo "$0: Bad argument to script." 
+      exit 1
+      ;;
+  esac
+done
+# Put rest of args in place to check all paths given.
+shift $((OPTIND-1))
+
+# Check for at least one path argument
 if [ "x$1" == "x" ]; then
-  echo "usage: checkNFS /path/to/check"
-  exit 255
-fi
- 
-unset RESULT
-
-# Verify that this ins not a hung NFS mount (or hung mount 
-# of any kind, i guess. Use a 10 second timeout. That should 
-# be plenty for an automount to take place if necessary and 
-# return something. The something should be the number 1
-# even if the path is non-existant.
-
-read -t10 RESULT < <( ls -ld $1 2>&1 | wc -l )
-
-# Collect the result of the read. 
-# 0 means it was successful, 1 means it timed out.
-READ=$?
-
-if [ "x$READ" == "x1" ]; then
-  echo "$1 appears to be inaccessible."
+  echo "$0: No path given."
   exit 1
 fi
-
-# If the read didn't time out, we need to ask:
-# "Is it *really* an NFS mounted path?"
-
-if ! findNfsMount $1; then
-  echo "$1 does not appear to be a path on an NFS mounted volume."
-  exit 2
+ 
+RETURN=0
+# Check the path
+if isAutofsMount $1 ; then
+  verbose "$1 appears to be a Broad AutoFS mounted Filesystem."
 else
-  echo "$1 appears to be a path on an NFS mounted volume."
-  exit 0
+  verbose "$1 does not appear to be a Broad AutoFS mounted Filesystem."
 fi
 
+if isAccessible $1; then
+  verbose "$1 is accessible."
+else
+  verbose "$1 is NOT accessible."
+  RETURN=1
+fi
 
+if isNfsMount $1; then
+  verbose "$1 is mounted via NFS."
+else
+  verbose "$1 is not a mounted NFS filesystem."
+fi
+
+exit $RETURN
