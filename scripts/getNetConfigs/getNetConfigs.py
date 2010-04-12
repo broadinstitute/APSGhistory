34581b8d6f4659d07c62dd66b56009bb49b5e322
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-04-12 16:04:34 +0000

    Trying to redirect stdout and stderr to /dev/null for ssh-keygen subprocess.
    git-svn-id: https://subversion01.broadinstitute.org/APSG@758 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/getNetConfigs/getNetConfigs.py
+++ scripts/getNetConfigs/getNetConfigs.py
@@ -38,7 +38,7 @@ def get_config(list,cmd,user_pass,en_pass,ip):
 	for item in list:
 		if not path.exists("%s/%s" % (smb_rdir,item)):
 			#Pre-run that removes SSH host key first
-			p=Popen(["ssh-keygen","-R",item],stdin=None,stdout=None)
+			Popen(["ssh-keygen","-R",item],stdin=None,stdout=open('/dev/null', 'w'),stderr=subprocess.STDOUT)
 			sleep(10)
 
 			p=Popen([cmd, item, user_pass, en_pass, username,ip], stdin=None, stdout=PIPE)
