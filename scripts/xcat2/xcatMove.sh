84f0f180a4ed1023d7518aa05929c1808092bd81
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-05-27 15:53:30 +0000

    Added ability to set $1 to comma separated list
    git-svn-id: https://subversion01.broadinstitute.org/APSG@937 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/xcat2/xcatMove.sh
+++ scripts/xcat2/xcatMove.sh
@@ -3,28 +3,42 @@
 #Set Variables
 USER="root"
 HOST="pm"
-NODE=$1
 
-if [ $(tabgrep $1 2> /dev/null | wc -l) -gt 0 ]; then
-	echo "$1 exists.  Exiting..."
-	exit 0
-fi
+NODE_LIST=$(echo "$1" | tr ',' ' ')
 
-MAC=$(ssh $USER@$HOST grep "$NODE-eth0" /opt/xcat/etc/mac.tab | cut -f2 -d' ')
-IP=$(ssh $USER@$HOST grep $NODE /opt/xcat/etc/hosts | cut -f1 -d' ')
-MODEL=$(ssh $USER@$HOST grep $NODE /opt/xcat/etc/nodemodel.tab | cut -f2 -d' ')
+for NODE in $NODE_LIST
+do
+	if [ $(tabgrep $NODE  2> /dev/null | wc -l) -gt 0 ]; then
+		echo "$NODE exists.  Skipping..."
+		continue
+	fi
 
-case $MODEL in
-	"DELL") 			GROUP="dell,farm,all";;
-	"LS21" | "LS20" | "HS20") 	GROUP="ibm,farm,all";;
-	"HP") 				GROUP="hp,farm,all";;
-	*) 				GROUP="farm,all";;
-esac
+	MAC=$(ssh $USER@$HOST grep "$NODE-eth0" /opt/xcat/etc/mac.tab | cut -f2 -d' ')
+	IP=$(ssh $USER@$HOST grep -w $NODE /opt/xcat/etc/hosts | cut -f1 -d' ')
+	MODEL=$(ssh $USER@$HOST grep -w $NODE /opt/xcat/etc/nodemodel.tab | cut -f2 -d' ')
 
-svnbuild nodeadd $NODE groups=$GROUP mac.interface=eth0 hosts.ip=$IP mac.mac=$MAC
-sleep 30
 
-makehosts $NODE
-makedhcp $NODE
+	case $MODEL in
+		"DELL") 			GROUP="dell,farm,all";;
+		"LS21" | "LS20" | "HS20")
+			GROUP="ibm,farm,all"
+			IBM_SLOT=$(ssh $USER@$HOST grep -w $NODE /opt/xcat/etc/mp.tab | cut -f2 -d,)
+			IBM_CHASSIS=$(ssh $USER@$HOST grep -w $NODE /opt/xcat/etc/mp.tab | cut -f1 -d, | awk '{print $2}')
+			;;
+		"HP") 				GROUP="hp,farm,all";;
+		*) 				GROUP="farm,all";;
+	esac
 
-ssh $USER@$HOST makedhcp -d $NODE
+	if [ $GROUP = "ibm,farm,all" ]; then
+		svnbuild nodeadd $NODE groups=$GROUP mac.interface=eth0 hosts.ip=$IP mac.mac=$MAC mp.mpa=$IBM_CHASSIS mp.id=$IBM_SLOT
+	else
+		svnbuild nodeadd $NODE groups=$GROUP mac.interface=eth0 hosts.ip=$IP mac.mac=$MAC
+	fi
+
+	sleep 30
+
+	makehosts $NODE
+	makedhcp $NODE
+
+	ssh $USER@$HOST makedhcp -d $NODE
+done
