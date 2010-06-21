132ee9b74fbd344afd6331c87172481f57d5c6cf
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-06-21 18:13:28 +0000

    Moved add and created rm
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1210 95e82132-2061-11dd-9cc4-33af95911ecc

---
+++ scripts/cacti/addCactiDevice.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+HOST=$1
+TREE=$2
+CSTRING=$3
+HOST_TEMPLATE=$4
+
+cd /var/lib/cacti/cli
+#Add Host
+HOST_TEMPLATE_ID=$(php -q add_device.php --list-host-templates | grep "$HOST_TEMPLATE" | awk '{print $1}')
+php -q add_device.php --description=$HOST --ip=$(host $HOST | cut -f1 -d' ') --template=$HOST_TEMPLATE_ID --community=$CSTRING --ping_method=udp
+HOST_ID=$(php -q add_tree.php --list-hosts | grep "$HOST" | awk '{print $1}')
+TREE_ID=$(php -q add_tree.php --list-trees | grep -i "$TREE" | awk '{print $1}')
+php -q add_tree.php --type=node --node-type=host --host-id=$HOST_ID --tree-id=$TREE_ID
+
+#Add Graphs
+php -q add_graphs.php --host-id=$HOST_ID --graph-type=cg --graph-template-id=4
+php -q add_graphs.php --host-id=$HOST_ID --graph-type=cg --graph-template-id=11
+php -q add_graphs.php --host-id=$HOST_ID --graph-type=cg --graph-template-id=43
+php -q add_graphs.php --graph-type=ds --host-id=$HOST_ID --graph-template-id=35 --snmp-query-id=1 --snmp-field=ifIP --snmp-query-type-id=13 --snmp-value=$(host $HOST | awk '{print $NF}') --graph-title="$HOST - Traffic"
+
+#Return you to your regularly scheduled program
+cd -
---
+++ scripts/cacti/rmCactiDevice.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+HOST=$1
+
+cd /var/lib/cacti/cli
+#Add Host
+HOST_ID=$(php -q remove_device.php --list-devices | grep -i $HOST | awk '{print $1}')
+php -q remove_device.php --device-id=$HOST_ID
+
+#Return you to your regularly scheduled program
+cd -
