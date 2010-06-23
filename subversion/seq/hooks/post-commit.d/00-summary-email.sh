6fc570ac4fcb61d4569b842caa427f9561cf44af
adb <adb@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-06-23 13:48:28 +0000

    broad.mit.edu -> broadinstitute.org
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1223 95e82132-2061-11dd-9cc4-33af95911ecc

--- subversion/seq/hooks/post-commit.d/00-summary-email.sh
+++ subversion/seq/hooks/post-commit.d/00-summary-email.sh
@@ -2,9 +2,9 @@
 
 # summarize commits of tests
 
-/broad/tools/scripts/svn-summary-email.rb "$@" 'test|selenium' '"Susan McDonough" <smcdonou@broad.mit.edu>'
+/broad/tools/scripts/svn-summary-email.rb "$@" 'test|selenium' '"Susan McDonough" <smcdonou@broadinstitute.org>'
 
 # (per RT #147945) summarize commits of files controlling inclusion of
 # third-party objects
 
-/broad/tools/scripts/svn-summary-email.rb "$@" '^main/sequence/.*/(profiles|pom).xml$' releng@broad.mit.edu
+/broad/tools/scripts/svn-summary-email.rb "$@" '^main/sequence/.*/(profiles|pom).xml$' releng@broadinstitute.org
