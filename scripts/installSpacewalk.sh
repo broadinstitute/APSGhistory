8da4ea22d3d40e9094dc6442957aa107ab1fa882
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-07-02 21:40:14 +0000

    Initial version
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1273 95e82132-2061-11dd-9cc4-33af95911ecc

---
+++ scripts/installSpacewalk.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Simple script to install spacewalk
+# 6/24/10
+
+#Auto-set arch
+BASEARCH=$(uname -i)
+
+#obtain required repos
+rpm -Uvh http://spacewalk.redhat.com/yum/1.0/RHEL/5/$BASEARCH/spacewalk-client-repo-1.0-2.el5.noarch.rpm
+rpm -Uvh http://download.fedora.redhat.com/pub/epel/5/$BASEARCH/epel-release-5-3.noarch.rpm
+
+#install the good stuff
+yum install rhn-client-tools rhn-check rhn-setup rhnsd m2crypto yum-rhn-plugin osad
+
+#register server on spacewalk
+rhnreg_ks --serverUrl=http://vspacewalk.broadinstitute.org/XMLRPC --activationkey=1-CentOSBroad
+
