84a3f9c6be4f167ee262c79963be368fdbca1643
adb <adb@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-07-08 16:56:10 +0000

    Tweak to start a new email whenever the current one gets close to 50 MB.
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1278 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/warn-users.rb
+++ scripts/warn-users.rb
@@ -3,33 +3,43 @@
 require 'find'
 require 'etc'
 
-email = {}
+$email = {}
+
+# Limits how much filename content we send per email; some headroom
+# short of our current 50 MB message size limit.
+EMAIL_SIZE_LIMIT = 49876543
+$email_size = {}
 
 def start_email(uid)
-    pwent = Etc.getpwuid(uid)
-    username, fullname = pwent.name, pwent.gecos
+  pwent = Etc.getpwuid(uid)
+  username, fullname = pwent.name, pwent.gecos
 
-    sendmail = IO.popen("/usr/sbin/sendmail -f apsg@broadinstitute.org #{username}@broadinstitute.org", 'w')
+#  sendmail = IO.popen("/usr/sbin/sendmail -f apsg@broadinstitute.org #{username}@broadinstitute.org", 'w')
+  sendmail = IO.popen("/usr/sbin/sendmail -f adb@broadinstitute.org adb@broadinstitute.org", 'w')
 
-    sendmail.puts "From: Research Computing resource usage alerts <help@broadinstitute.org>",
-                  "To:   \"#{fullname}\" <#{username}@broadinstitute.org>",
-                  "Subject: /broad/shptmp deletion warning",
-		  "",
-		  "The following files you own in /broad/shptmp have not been accessed or modified in 7 days",
-		  "and will be deleted 7 days from now:",
-		  ""
+  sendmail.puts header = <<HEADER
+From: Research Computing resource usage alerts <help@broadinstitute.org>
+To:   \"#{fullname}\" <#{username}@broadinstitute.org>
+Subject: /broad/shptmp deletion warning
 
-    return sendmail
+The following files you own in /broad/shptmp have not been accessed or modified in 7 days
+and will be deleted 7 days from now:
+
+HEADER
+  $email_size[uid] = header.size
+
+  return sendmail
 end
 
 Find.find('/broad/shptmp/') { |path|
 
-   Find.prune if File.basename(path) == '.snapshot'
-   status = File.lstat(path) rescue next
+  Find.prune if File.basename(path) == '.snapshot'
+  status = File.lstat(path) rescue next
 
-   next if status.directory?
+  next if (status.directory? or
+           %w[mtime ctime atime].detect { |time| Time.now - status.send(time) < 7*86400 })
 
-   next if %w[mtime ctime atime].detect { |time| Time.now - status.send(time) < 7*86400 }
-
-   (email[status.uid] ||= start_email(status.uid)).puts path    
+  $email.delete(status.uid) if ($email[status.uid] and
+                                $email_size[status.uid] >= EMAIL_SIZE_LIMIT)
+  ($email[status.uid] ||= start_email(status.uid)).puts path
 }
