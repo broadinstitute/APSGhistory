78e96832c09b16343ec8c7640186a5e0417a6063
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-07-09 17:27:45 +0000

    Added new password confirmation and debug mode (off by default).  If debug mode is not enabled, it's possible that there will be no output.  Neither of these features have been tested yet, since I already finished updating the CMCs.
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1286 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/dell/changeCMCPass.py
+++ scripts/dell/changeCMCPass.py
@@ -7,17 +7,24 @@ from getpass import getpass
 hosts = argv[1].split(',')
 opass = getpass("Current Password:")
 npass = getpass("New Password:")
+npassConfirm = getpass("Confirm New Password:")
+debugMode = False
 
-for host in hosts:
-	child = pexpect.spawn("ssh %s" % host)
-	child.logfile = sys.stdout
-	child.expect("root@%s's password:" % host)
-	child.sendline("%s" % opass)
-	child.expect('Welcome')
-	child.expect('$')
-	child.sendline("racadm config -g cfgUserAdmin -o cfgUserAdminPassword -i 1 \'%s\'" % npass)
-	child.expect('$')
-	child.sendline("racreset")
-	child.expect('$')
-	child.sendline("exit")
-	child.expect(pexpect.EOF)
+if npass == npassConfirm:
+	for host in hosts:
+		child = pexpect.spawn("ssh %s" % host)
+		if debugMode:
+			child.logfile = sys.stdout
+		child.expect("root@%s's password:" % host)
+		child.sendline("%s" % opass)
+		child.expect('Welcome')
+		child.expect('$')
+		child.sendline("racadm config -g cfgUserAdmin -o cfgUserAdminPassword -i 1 \'%s\'" % npass)
+		child.expect('$')
+		child.sendline("racreset")
+		child.expect('$')
+		child.sendline("exit")
+		child.expect(pexpect.EOF)
+else:
+	print "Passwords do not match.  Exiting..."
+	exit(1)
