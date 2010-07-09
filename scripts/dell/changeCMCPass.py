2bd204e18d38988ba5bf65acb7dd7152f5cd1b5a
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-07-09 17:09:52 +0000

    Converted hosts variable to array.  Script now allows comma-separated host list.
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1285 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/dell/changeCMCPass.py
+++ scripts/dell/changeCMCPass.py
@@ -4,19 +4,20 @@ import sys
 from sys import argv,exit
 from getpass import getpass
 
-host = argv[1]
+hosts = argv[1].split(',')
 opass = getpass("Current Password:")
 npass = getpass("New Password:")
 
-child = pexpect.spawn("ssh %s" % host)
-child.logfile = sys.stdout
-child.expect("root@%s's password:" % host)
-child.sendline("%s" % opass)
-child.expect('Welcome')
-child.expect('$')
-child.sendline("racadm config -g cfgUserAdmin -o cfgUserAdminPassword -i 1 \'%s\'" % npass)
-child.expect('$')
-child.sendline("racreset")
-child.expect('$')
-child.sendline("exit")
-child.expect(pexpect.EOF)
+for host in hosts:
+	child = pexpect.spawn("ssh %s" % host)
+	child.logfile = sys.stdout
+	child.expect("root@%s's password:" % host)
+	child.sendline("%s" % opass)
+	child.expect('Welcome')
+	child.expect('$')
+	child.sendline("racadm config -g cfgUserAdmin -o cfgUserAdminPassword -i 1 \'%s\'" % npass)
+	child.expect('$')
+	child.sendline("racreset")
+	child.expect('$')
+	child.sendline("exit")
+	child.expect(pexpect.EOF)
