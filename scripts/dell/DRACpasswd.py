e594888d100619bb70c1440265f5a532f895806a
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-07-12 18:39:02 +0000

    Initial Version
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1291 95e82132-2061-11dd-9cc4-33af95911ecc

---
+++ scripts/dell/DRACpasswd.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import pexpect
+import sys
+from sys import argv,exit
+from getpass import getpass
+
+hosts = argv[1].split(',')
+opass = getpass("Current Password:")
+npass = getpass("New Password:")
+npassConfirm = getpass("Confirm New Password:")
+debugMode = True
+
+if npass == npassConfirm:
+	for host in hosts:
+		child = pexpect.spawn("ssh %s" % host)
+		if debugMode:
+			child.logfile = sys.stdout
+		child.expect("root@%s's password:" % host)
+		child.sendline("%s" % opass)
+		child.expect('$')
+		child.sendline("racadm config -g cfgUserAdmin -o cfgUserAdminPassword -i 2 \'%s\'" % npass)
+		child.expect('$')
+		child.sendline("racadm racreset")
+		child.expect('$')
+		child.sendline("exit")
+		child.expect(pexpect.EOF)
+else:
+	print "Passwords do not match.  Exiting..."
+	exit(1)
