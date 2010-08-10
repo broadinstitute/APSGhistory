7bd1fc5f1ccd526ae40bf4b3abbfa66e527e9768
adb <adb@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-08-10 19:41:35 +0000

    refactor gently
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1382 95e82132-2061-11dd-9cc4-33af95911ecc

--- binary-tools/boundedFind/boundedFind.hs
+++ binary-tools/boundedFind/boundedFind.hs
@@ -1,34 +1,36 @@
 #!/usr/bin/env runhaskell
 
-module Main (main, processPath, unsafeGracefulProcess, deMaybe, steps, showResults) where
+import Directory
 
+import Data.List ((\\), intercalate)
+import Data.Maybe (maybeToList)
+
+import System (getArgs)
+import System.IO
+import System.IO.Unsafe (unsafePerformIO, unsafeInterleaveIO)
 import System.FilePath.Posix (combine)
 import System.Posix.Files
-import Directory
-import Data.List ((\\), intercalate)
-import System.IO.Unsafe (unsafePerformIO, unsafeInterleaveIO)
-import System (getArgs)
-import Data.Maybe (maybeToList)
-import Time
-import Control.Monad
-import System.IO
 
 -- Suppose we have a collection of tasks, and performing each task
 -- will produce some results and some more tasks.  The steps
 -- function starts with a task list, performs the first task, reports
 -- the results and the new task list, and continues.
 
-takeUntil test [] = []
-takeUntil test (a:b:bs) | (test b) && not (test a) = [a,b]
-takeUntil test (a:bs) = (a : takeUntil test bs)
-
 steps step tasks = takeUntil (null . snd) $ process tasks where
-           process (task:tasks) = let (r, t) = step task in ((r, t ++ tasks) : process (t ++ tasks))
+           process (task:tasks) = let (r, t) = step task
+                                  in ((r, t ++ tasks) : process (t ++ tasks))
+
+-- Like takeWhile (not . f), but reports the 
+takeUntil test [] = []
+takeUntil test (a:as) | test a    = [a]
+                      | otherwise = (a : takeUntil test as)
 
 instance Show FileStatus where
          show s = intercalate "/" $ zipWith ($) [show . fileSize, show . fileMode,
-                                   show . fileOwner, show . fileGroup,
-                                   show . modificationTime, show . accessTime, show . statusChangeTime] (repeat s)
+                                                 show . fileOwner, show . fileGroup,
+                                                 show . modificationTime,
+                                                 show . accessTime,
+                                                 show . statusChangeTime] (repeat s)
 
 data FileInfo = FI { status :: FileStatus, path :: FilePath } deriving (Show)
 
@@ -36,12 +38,10 @@ getFileInfo p = do s <- getSymbolicLinkStatus p
                    return FI { status = s, path = p }
 
 processPath p = do info <- getFileInfo p
-                   kids <- if isDirectory $ status info then getDirectoryContents $ path info else return []
+                   kids <- if isDirectory $ status info
+                           then getDirectoryContents $ path info
+                           else return []
                    return (info, map (combine p) $ kids \\ [".", ".."])
-
-unsafeGracefulProcess p = unsafePerformIO $ catch (return . (\(r,t) -> (Just r, t)) =<< processPath p) (\e -> return (Nothing, []))
-
-getLengths = map (\(r,t) -> (r,length t))
 
 promoteMaybe (Nothing, t) = Nothing
 promoteMaybe (Just r, t)  = Just (r,t)
@@ -54,4 +54,7 @@ deMaybe = concatMap (maybeToList.promoteMaybe)
 
 main = do (checkpointPath : count : paths) <- getArgs
        	  withFile checkpointPath WriteMode $ \h ->
-	  	   sequence_ $ showResults h $ take (read count) $ deMaybe $ steps unsafeGracefulProcess paths
+	   sequence_ $ showResults h $ take (read count) $ deMaybe $
+            steps (unsafePerformIO . (graceful processPath)) paths where
+             graceful p i = catch (return . (\(r,t) -> (Just r, t)) =<< p i)
+                                  (\e -> return (Nothing, []))
