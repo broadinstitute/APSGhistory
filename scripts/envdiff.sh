785e05044691d122f28c2aef6ac29d27754c1125
adb <adb@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-09-01 20:51:35 +0000

    add handler for _dk_inuse like for paths
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1573 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/envdiff.sh
+++ scripts/envdiff.sh
@@ -1,11 +1,13 @@
-#!/bin/bash -x
+#!/bin/bash
 
 # Take the output of "env" from two contexts and compare them with
 # "diff", after doing some preprocessing to make differences in path
-# variables easier to interpret.
+# variables and _dk_inuse easier to interpret.  Do a stable sort on
+# variable names so that related lines come up next to each other.
 
 expand_paths() {
-    perl -pe 's/^([^=]+)=(.*?:.*?)\s*$/join "", map { "$1 $_\n" } split ":", $2/e' $*
+    perl -pe 's/^([^=]+)=(.*?:.*?)\s*$/join "", map { "$1 $_\n" } split ":", $2/e' $* |
+      perl -pe 's/^(_dk_inuse)=(\S+?\s.*?)\s*$/join "", map { "$1 $_\n" } split " ", $2/e'
 }
 
-diff -U0 <(sort $1 | expand_paths) <(sort $2 | expand_paths) | grep '^[-+][^-+]'
+diff -dU0 <(sort $1 | expand_paths) <(sort $2 | expand_paths) | grep '^[-+][^-+]' | sort -sk1.2,1.999
