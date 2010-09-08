de2b2b813e6d799623cb7bbd4c9311857005ad43
adb <adb@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-09-08 18:31:05 +0000

    reorient toward pregenerating a long list of assignments
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1627 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/draw-straws.rb
+++ scripts/draw-straws.rb
@@ -1,6 +1,22 @@
 #!/usr/bin/env ruby
 
-count   = ARGV.shift.to_i
-victims = ARGV.shuffle[0,count]
+require 'time'
 
-puts victims
+victims_per_day = ARGV.shift.to_i rescue 1
+
+days = ARGV.shift.to_i if ARGV[0] and ARGV[0].to_i > 0
+
+def straws(n)
+  ARGV.shuffle[0,n].join(' ')
+end
+
+if days.nil?
+  puts straws(victims_per_day)
+else
+  time = Time.now
+
+  days.times {
+    puts time.strftime("%Y-%m-%d ") + straws(victims_per_day)
+    time += 86400 # 1 day in seconds
+  }
+end
