d52301767751f87e650ddd2d1f673f872fbdaacd
adb <adb@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-09-14 20:02:14 +0000

    update path to summary script to use /broad/tools/scripts/s
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1641 95e82132-2061-11dd-9cc4-33af95911ecc

--- subversion/CancerGenomeAnalysisData/hooks/post-commit.d/00-summary-email.rb
+++ subversion/CancerGenomeAnalysisData/hooks/post-commit.d/00-summary-email.rb
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/broad/tools/scripts/svn-summary-email.rb "$@" '' cga-svn@broad.mit.edu
+/broad/tools/scripts/s Ruby-1.8 ruby /broad/tools/scripts/svn-summary-email.rb "$@" '' cga-svn@broad.mit.edu
