bc8701dcacb753840e3f1e843b5dcac756922f3f
adb <adb@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-09-14 20:02:20 +0000

    update path to summary script to use /broad/tools/scripts/s
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1642 95e82132-2061-11dd-9cc4-33af95911ecc

--- subversion/Tribble/hooks/post-commit.d/00-summary-email.rb
+++ subversion/Tribble/hooks/post-commit.d/00-summary-email.rb
@@ -3,4 +3,4 @@
 REPOS="$1"
 REV="$2"
 
-/broad/svn/svn/tools/hook-scripts/commit-email.pl "$REPOS" "$REV" --from svn@broadinstitute.org -s "SVN: " tribble_svn_list@broadinstitute.org
+/broad/svn/svn/tools/hook-scripts/commit-email.sh "$REPOS" "$REV" --from svn@broadinstitute.org -s "SVN: " tribble_svn_list@broadinstitute.org
