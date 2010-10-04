57f44df2d82ce978973f254a08af562772b5c841
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-10-04 16:19:41 +0000

    Updated labels for more accurate description
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1676 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/lsfCentosInv.sh
+++ scripts/lsfCentosInv.sh
@@ -5,5 +5,6 @@ reuse -q LSF
 for OS_TYPE in $(lshosts -s ostype | awk '$2 ~ /CENT/ {print $2}' | sort -u); do
 	OUTPUT=$(~ali/bashcalc.sh $(lshosts -s ostype | grep $OS_TYPE | wc -l)/$(lshosts -s ostype | grep CENT | wc -l))
 	OUTPUT=$(~ali/bashcalc.sh $OUTPUT*100)
-	echo "$OUTPUT% of CentOS is $OS_TYPE"
+	echo "$OUTPUT% ($(lshosts -s ostype | grep $OS_TYPE | wc -l)/$(lshosts -s ostype | grep -v RESOURCE | wc -l)) of LSF is $OS_TYPE"
 done
+echo "$(~ali/bashcalc.sh $(~ali/bashcalc.sh $(lshosts -s ostype | grep CENT | wc -l)/$(lshosts -s ostype | grep -v RESOURCE | wc -l))*100)% ($(lshosts -s ostype | grep CENT | wc -l)/$(lshosts -s ostype | grep -v RESOURCE | wc -l)) of LSF is CentOS"
