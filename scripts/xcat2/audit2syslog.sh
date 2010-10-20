02002ac63083b7825d2fb3028d63bc4285391e3c
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-10-20 17:40:37 +0000

    Initial version
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1691 95e82132-2061-11dd-9cc4-33af95911ecc

---
+++ scripts/xcat2/audit2syslog.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+RAW_LOGS=$(/opt/xcat/sbin/tabprune auditlog -V -a | grep -v '^#')
+CLEAN_LOGS=$(echo "$RAW_LOGS" | awk -F, '{printf "recid:" $1 ", audittime:" $2 ", userid:" $3 ", clientname:" $4 ", clienttype:" $5 ", command:" $6 ", noderange:" $7 ", args:" $8 ", status:" $9 "\n"}' | tr -d '"')
+echo "$CLEAN_LOGS" | /bin/logger -t xCAT-Audit
