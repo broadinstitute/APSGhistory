679544bcd21bfba88e56ebcf63934df8bc4174f0
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-10-22 06:10:46 +0000

    Minor performance enhancement.  Still a long way to go.
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1748 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/selCollector.sh
+++ scripts/selCollector.sh
@@ -4,8 +4,9 @@ USER=service
 GREP_EXCEPTIONS="log cleared"
 
 for CMC in brsa{50..88}; do
+	HOSTS=$(ssh service@$CMC getslotname)
 	for i in {1..16}; do
-		HOST=$(ssh $USER@$CMC getslotname -i $i)
+		HOST=$(echo "$HOSTS" | grep -w $i | awk '{print $NF}')
 		MESSAGE=$(ssh $USER@$CMC getsel -m server-$i | grep -v "$GREP_EXCEPTIONS")
 		if [ -n "$MESSAGE" ]; then
 			echo -e "$HOST\n$MESSAGE"
