f809fde7a31754154dcf9d665803f1e2de0aa3f1
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-10-25 22:59:03 +0000

    Nothing of import
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1768 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/selCollector.sh
+++ scripts/selCollector.sh
@@ -3,13 +3,21 @@
 USER=service
 GREP_EXCEPTIONS="log cleared"
 
-for CMC in brsa{50..88}; do
-	HOSTS=$(ssh service@$CMC getslotname)
-	for i in {1..16}; do
-		HOST=$(echo "$HOSTS" | grep -w $i | awk '{print $NF}')
-		MESSAGE=$(ssh $USER@$CMC getsel -m server-$i | grep -v "$GREP_EXCEPTIONS")
-		if [ -n "$MESSAGE" ]; then
-			echo -e "$HOST\n$MESSAGE"
-		fi
+START=$1
+END=$(($START+9))
+MAX_BRSA=88
+x=0 # Correct value gets set in for loop
+
+while [ $x -le $MAX_BRSA ]; do
+	for x in $(seq $START $END); do
+		CMC="brsa$x"
+		HOSTS=$(ssh $USER@$CMC getslotname)
+		for i in {1..16}; do
+			HOST=$(echo "$HOSTS" | grep -w $i | awk '{print $NF}')
+			MESSAGE=$(ssh $USER@$CMC getsel -m server-$i | grep -v "$GREP_EXCEPTIONS")
+			if [ -n "$MESSAGE" ]; then
+				echo -e "$HOST\n$MESSAGE"
+			fi
+		done
 	done
 done
