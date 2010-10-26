df4eaa8e875451656a78ffb6c445643be42dc5f3
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-10-26 19:01:48 +0000

    Initial version
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1772 95e82132-2061-11dd-9cc4-33af95911ecc

---
+++ scripts/dell/CMC/svctags.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+
+
+USER=service
+
+while [ $# -ge 1 ]; do
+	HOST=$1
+	SVCTAG=$(ssh $USER@$HOST getsvctag -m chassis 2> /dev/null | awk '$1 ~ /Chassis/ {print $2}')
+
+	if [ -z $SVCTAG ]; then
+		echo "SSH Failed. Check username." && exit 1
+	fi
+
+	echo -e "$HOST\t$SVCTAG"  
+	shift
+done
