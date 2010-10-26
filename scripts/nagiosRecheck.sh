ccec1e7b1f2bdb83514d8d4ad7001b54959f5162
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-10-26 16:02:02 +0000

    $HOST is now comma separated and $USER is now optional
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1770 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/nagiosRecheck.sh
+++ scripts/nagiosRecheck.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#https://systems.broadinstitute.org/nagios/cgi-bin/cmd.cgi?cmd_typ=96&host=node1574&force_check
-#https://systems.broadinstitute.org/nagios/cgi-bin/cmd.cgi?cmd_typ=17&host=node1606$force_check
 
 #Functions
 startDate(){
@@ -8,9 +6,35 @@ startDate(){
         echo $START_DATE
 }
 
-#Arg Variable Block
-HOST=$1
-USER=$2
+#HOST_LIST is only required argument
+HOST_LIST=$(echo "$1" | tr ',' ' ')
+
+
+# Here we define Usage -- define once, print many
+USAGE="Usage: `basename $0` \$HOSTS \$USER"
+
+# error codes as defined in /usr/include/sysexits.h
+E_OK=0                  # successful termination
+E_USAGE=64              # command line usage error 
+E_NOINPUT=66            # cannot open input
+E_NOHOST=68             # host not found
+E_UNAVAILABLE=69        # service unavailable
+
+case $# in
+        1)
+		USER=$(printenv USER)
+                ;;
+        2)
+                USER=$2
+                ;;
+        *)
+		echo 1>&2 $USAGE
+		exit $E_USAGE
+                ;;
+esac
+
+echo $USER
+exit 0
 
 #Password Input
 stty -echo
@@ -20,5 +44,7 @@ stty echo
 #App Variables
 NAGIOS_URL="https://systems.broadinstitute.org/nagios/cgi-bin/cmd.cgi"
 
-curl -u $USER:$PASSW -d "cmd_typ=96&cmd_mod=2&host=$HOST&start_time=$(startDate)&force_check&&btnSubmit=Commit" "$NAGIOS_URL"
-curl -u $USER:$PASSW -d "cmd_typ=17&cmd_mod=2&host=$HOST&start_time=$(startDate)&force_check&&btnSubmit=Commit" "$NAGIOS_URL"
+for HOST in $HOST_LIST; do
+	curl -u $USER:$PASSW -d "cmd_typ=96&cmd_mod=2&host=$HOST&start_time=$(startDate)&force_check&&btnSubmit=Commit" "$NAGIOS_URL"
+	curl -u $USER:$PASSW -d "cmd_typ=17&cmd_mod=2&host=$HOST&start_time=$(startDate)&force_check&&btnSubmit=Commit" "$NAGIOS_URL"
+done
