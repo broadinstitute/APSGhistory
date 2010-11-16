610bc783feb25f50ecd8adacfd48234afeb44378
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-11-16 00:00:59 +0000

    Initial version
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1790 95e82132-2061-11dd-9cc4-33af95911ecc

---
+++ scripts/cacti/cactiMv.sh
@@ -0,0 +1,36 @@
+#/bin/bash
+
+# $Id$
+
+MYSQL_BIN="/usr/bin/mysql"
+CLI_DIR="/var/lib/cacti/cli"
+USER="cacti_dev"
+PASSWORD="ELnSrYXr"
+DB_SERVER="itdb"
+DATABASE="cacti_dev"
+SRC_TREE_NAME=$1
+DEST_TREE_NAME=$2
+HOSTNAME=$3
+
+HOST=$(php -q $CLI_DIR/add_tree.php --list-hosts | grep -w "$HOSTNAME" | awk '{print $1}')
+SRC_TREE=$(php -q $CLI_DIR/add_tree.php --list-trees | grep -i "$SRC_TREE_NAME" | awk '{print $1}')
+DEST_TREE=$(php -q $CLI_DIR/add_tree.php --list-trees | grep -i "$DEST_TREE_NAME" | awk '{print $1}')
+
+SQL_CODE="
+DELETE FROM graph_tree_items
+WHERE host_id=${HOST}
+	AND graph_tree_id=${SRC_TREE};"
+	
+SQL_CMD="$MYSQL_BIN -e \"${SQL_CODE}\" -u${USER} -p${PASSWORD} -h ${DB_SERVER} ${DATABASE}"
+
+TREE_ID=$(php -q $CLI_DIR/add_tree.php --list-trees | grep -i "$DEST_TREE_NAME" | awk '{print $1}')
+if [ -n $TREE_ID ]; then
+        if [[ $(php -q $CLI_DIR/add_tree.php --list-nodes --tree-id=$TREE_ID | grep -wc "$HOSTNAME") -eq 0 ]]; then
+                php -q $CLI_DIR/add_tree.php --type=node --node-type=host --host-id=$HOST --tree-id=$DEST_TREE
+		echo "$SQL_CMD" | bash
+        else
+                echo "Skipping add_tree for $HOST.  Already exists in tree, and Cacti is stupid."
+        fi
+else
+        echo "Something is wrong with tree.  Does not compute."
+fi
