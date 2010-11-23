fcd3f203ad197973d7a71479734c803380f93596
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-11-23 21:42:14 +0000

    Cleaned up output a bit
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1801 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/xcat2/nodelocateGen.py
+++ scripts/xcat2/nodelocateGen.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 # $Id$
-import sys,subprocess,string,os,shutil
+import sys,subprocess,string,os,shutil,glob
 
-sys.path.append('/broad/tools/scripts/')
+ssh_lib_path = '/broad/tools/scripts'
+sys.path.append(ssh_lib_path)
 import ssh
+
+ofile_path = '/tmp/nodepos'
 
 for cmcNum in range(50,90):
 	cmc = "brsa%s" % cmcNum
 	try:
-		tempFile = '/tmp/nodepos/temp-%s' % cmc
+		tempFile = '%s/temp-%s' % (ofile_path,cmc)
 		file = open (tempFile, 'w')
 		s = ssh.Connection(cmc,'service')
 		output = s.execute('getslotname')
@@ -18,15 +21,29 @@ for cmcNum in range(50,90):
 			itemList = item.split()
 
 			if "Host" not in item and len(itemList) < 4:
-				if len(itemList) < 3:
-					file.write('"%s",,,"%s","%s",,,' %(itemList[1],cmc,itemList[0]))
-				else:
-					file.write('"%s",,,"%s","%s",,,' %(itemList[2],cmc,itemList[0]))
+				if len(itemList) == 3:
+					file.write('"%s",,,"%s","%s",,,\n' %(itemList[2],cmc,itemList[0]))
 	except Exception:
 		file.close()
 		os.remove(tempFile)
 		continue
 	else:
 		file.close()
-		file = '/tmp/nodepos/%s' % cmc
+		file = '%s/%s'  % (ofile_path,cmc)
 		shutil.move(tempFile,file)
+
+os.remove('%s/ssh.pyc' % ssh_lib_path)
+
+destination = open('%s/nodepos.csv' % ofile_path, 'w')
+for filename in glob.glob(os.path.join(ofile_path, 'brsa*')):
+    shutil.copyfileobj(open(filename, 'r'), destination)
+destination.close()
+
+file = open('%s/nodepos.csv' % ofile_path, 'r')
+lines = file.readlines()
+file.close()
+ofile = open('%s/nodepos.csv' % ofile_path, 'w')
+ofile.write("#node,rack,u,chassis,slot,room,comments,disable\n")
+for line in sorted(lines, key = str.lower):
+        ofile.write(line)
+ofile.close()
