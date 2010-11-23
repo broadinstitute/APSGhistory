0db9355c092351f69971196ab23b0baef1ce12c3
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-11-23 23:01:54 +0000

    Changed logger category to local0
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1802 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/xcat2/audit2syslog.sh
+++ scripts/xcat2/audit2syslog.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+# $Id$
 
 RAW_LOGS=$(/opt/xcat/sbin/tabprune auditlog -V -a | grep -v '^#')
 CLEAN_LOGS=$(echo "$RAW_LOGS" | awk -F, '{printf "recid:" $1 ", audittime:" $2 ", userid:" $3 ", clientname:" $4 ", clienttype:" $5 ", command:" $6 ", noderange:" $7 ", args:" $8 ", status:" $9 "\n"}' | tr -d '"')
-echo "$CLEAN_LOGS" | /bin/logger -t xCAT-Audit
+echo "$CLEAN_LOGS" | /bin/logger -t xCAT-Audit -p local0.notice
