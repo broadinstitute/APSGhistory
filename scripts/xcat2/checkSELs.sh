77e2f5a182b85d2b04e9e94267203e73577a2588
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-11-23 23:30:35 +0000

    Initial version
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1804 95e82132-2061-11dd-9cc4-33af95911ecc

---
+++ scripts/xcat2/checkSELs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# $Id$
+
+VENDORS="dell ibm"
+LOGPATH="/tmp/events/"
+
+for VENDOR in VENDORS; do
+	for HOST in $(nodels $VENDOR); do 
+		/opt/xcat/bin/reventlog $HOST > $LOGPATH/$VENDOR/$HOST.log 2>&1
+		cat $LOGPATH/$VENDOR/$HOST.log | /bin/logger -t SEL -p local1.error
+		sleep 2
+	done
+done
