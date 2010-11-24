1b9d2ee100d7946e24f4c4ccaf4678e9bec27d45
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2010-11-24 00:13:13 +0000

    Initial Version
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1807 95e82132-2061-11dd-9cc4-33af95911ecc

---
+++ scripts/dell/getFlexAddr.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# $Id$
+import sys
+
+ssh_lib_path = '/broad/tools/scripts'
+sys.path.append(ssh_lib_path)
+import ssh
+
+for cmcNum in range(50,90):
+	cmc = "brsa%s" % cmcNum
+	print "\n"
+	print cmc
+
+	try:
+		s = ssh.Connection(cmc,'service')
+		output = s.execute('getflexaddr')
+		s.close()
+
+		for item in output:
+			item = item.rstrip()
+			if "enabled" not in item.lower():
+				print item
+	except Exception:
+		continue
