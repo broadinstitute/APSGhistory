b8583359c18819f1d1a2ddaf2a2325874eb85c58
jlchang <jlchang@95e82132-2061-11dd-9cc4-33af95911ecc>
2011-04-26 16:53:39 +0000

    This line, and those below, will be ignored--
    D    sync_mus_test.py
    D    sync_mus.py
    D    sync_monDomV5_farm.py
    A    svnlook
    M    parallel-cleanup.sh
    D    sync_monDom_lead.py
    D    sync_mus_lead.py
    M    mrtgdf
    D    sync_monDomV5_bigmem.py
    
    
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1885 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/parallel-cleanup.sh
+++ scripts/parallel-cleanup.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-FS=`perl -e 'print join "--", map { s./.-.g; $_ } @ARGV' "$@"`
+FS=`perl -e 'print join "--", map { s./.-.g; s.^-..; $_ } @ARGV' "$@"`
 
 GROUP=${LSB_JOBGROUP:-/apsg/cleanup/$FS}
 
-OUT=${LSB_OUTPUTFILE:-$FS.`date --iso`.log}
+JID=${LSB_JOBID:-root}
+
+# output file for child jobs
+OUT=${LSB_OUTPUTFILE:-$(mkdir -p /sysman/scratch/apsg/cleanup/log/$FS ; echo /sysman/scratch/apsg/cleanup/log/$FS/`date +%s`)}.$JID
 
 bgadd $GROUP >/dev/null 2>&1
-bgmod -L 50 $GROUP >/dev/null 2>&1
+bgmod -L 100 $GROUP >/dev/null 2>&1
 
 AGE=14
 
 RES='rusage['`/broad/tools/scripts/io_resource_for_file "$@" | sort | uniq | perl -0777ne 'print join ",", map { "$_=1" } split'`']'
 
-BSUB="bsub -g $GROUP -o /sysman/scratch/apsg/cleanup/$OUT -R $RES"
+BSUB="bsub -q hour -W 3:30 -u adb -g $GROUP -o $OUT -R $RES"
 
 TEST="( -mtime +$AGE -ctime +$AGE -atime +$AGE )"
 
@@ -21,4 +24,7 @@ RECURSE="-execdir $BSUB $0 {} +"
 
 BIGDIR='( -type d ( -size +10k -o -links +50 ) )'
 
-exec find "$@" -nowarn -mindepth 1 \( $BIGDIR $RECURSE -prune \) -o \( $TEST -fprintf >(xargs -I{} $BSUB find {} $TEST -delete) \"%p\n\" \)
+# The "-wholename" bit is so we don't parallelize below a certain
+# depth.
+
+exec find "$@" -nowarn -mindepth 1 \( $BIGDIR $RECURSE -prune \) -o \( \( $TEST -o \( -wholename './*/*' -type d \) \) \( -type d -fprintf >(xargs -0 -I{} $BSUB find {} $TEST -ls -delete) '%p\0' -o -type f -execdir rm {} + \) -prune \)
