caa37091078535b6e2e9f0fbe3f37860f3098090
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2011-04-26 21:08:42 +0000

    Completely overhauled.  The script now runs in under a minute usually, and has consistantly produce the right host list.  As part of this, I've removed the build.host.listing, since it was only wasting space.
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1887 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/xcat2/update.py
+++ scripts/xcat2/update.py
@@ -1,106 +1,71 @@
 #!/usr/bin/env python
-import re,sys
+import sys,re,os
 from subprocess import *
-from threading import *
 from tempfile import NamedTemporaryFile
 
-
-
-def prepareHosts(ip,mac,host,groups,chassis,slot):
-    if len(chassis)>0 and len(slot)>0:
-        hostInfo="nodeadd {0} groups={1} mac.interface=eth0 hosts.ip={2} mac.mac={3} mp.mpa={4} mp.id={5}".format(host,groups,ip,mac,chassis,slot)
+#Functions
+def addHosts(line):
+    fields=line.split()[1].split('|')
+    (ip,mac,node,groups,mpa,id)=fields[0:6]
+    if len(mpa)>0 and len(id)>0:
+        nodeaddCMD="nodeadd {0} groups={1} mac.interface=eth0 hosts.ip={2} mac.mac={3} mp.mpa={4} mp.id={5}".format(node,groups,ip,mac,mpa,id)
     else:
-        hostInfo="nodeadd {0} groups={1} mac.interface=eth0 hosts.ip={2} mac.mac={3}".format(host,groups,ip,mac)
+        nodeaddCMD="nodeadd {0} groups={1} mac.interface=eth0 hosts.ip={2} mac.mac={3}".format(node,groups,ip,mac)
+        
+    Popen(nodeaddCMD.split(),stdout=PIPE,stderr=PIPE).communicate()
 
-    tmpBhl.write("{0}|{1}|{2}|{3}|{4}|{5}|\n".format(ip,mac,host,groups,chassis,slot))
-
-    host_thread=Thread(target=configureHosts, args=(hostInfo,))
-    host_thread.start()
-    host_thread.join()
-
-def configureHosts(command):
-    masterFile="/sysman/install/broad/master.host.listing"
-    buildFile="/broad/tools/scripts/xcat2/build.host.listing"
-    Popen(command.split(),stdout=PIPE,stderr=PIPE).communicate()
+def removeHosts(line):
+    fields=line.split('|')
+    node=fields[2]
+    nodermCMD="noderm {0}".format(node)
     
-if __name__ == '__main__':
-    masterFile="/sysman/install/broad/master.host.listing"
-    buildFile="/broad/tools/scripts/xcat2/build.host.listing"
-    bhlExists=True
-    mhlList=[]
-    buildList=[]
-    tmpBhl=NamedTemporaryFile(delete=False)
+    Popen(nodermCMD.split(),stdout=PIPE,stderr=PIPE).communicate()
+#Commands
+lsdefAttr='ip,mac,groups,mpa,id'
+lsdefCMD=['lsdef','-t','node','-i',lsdefAttr]
+lsdef=Popen(lsdefCMD,stdout=PIPE).communicate()[0].split('\n\n')
+
+#Files
+buildFile=NamedTemporaryFile(delete=False)
+buildFileName=buildFile.name
+masterHost='/sysman/install/broad/master.host.listing'
+
+for line in lsdef:
+    if len(line.split('\n'))==0:continue
+    #print line.split('\n')
+    for entry in line.split('\n'):
+        if len(entry.rstrip())==0:continue
+        if 'Object' in entry: hostName=entry.split(':')[1].lstrip(' ')
+        if 'groups=' in entry: groups=entry.split('=')[1]
+        if 'id=' in entry: id=entry.split('=')[1]
+        if 'ip=' in entry: ip=entry.split('=')[1]
+        if 'mac=' in entry: mac=entry.split('=')[1]
+        if 'mpa=' in entry: mpa=entry.split('=')[1]
+    buildFile.write("#xCAT# {0}|{1}|{2}|{3}|{4}|{5}|\n".format(ip,mac,hostName,groups,mpa,id))
+buildFile.close()
+
+buildFile=open(buildFileName,'r')
+buildFileList=buildFile.readlines()
+
+mhl=open(masterHost,'r')
+mhlList=[]
+
+for line in mhl:
+    if not re.match('#xCAT#',line):continue
+    mhlList.append(line)
+
+for line in buildFileList:
+    if line in mhlList: continue
+    print("To be removed: {0}".format(line.rstrip()))
+    removeHosts(line)
     
-    try:
-        with open(buildFile) as f0:
-            bhl=f0.readlines()
-    except IOError:
-        bhlExists=False
+for line in mhlList:
+    if line in buildFileList: continue
+    print("To be added/modified: {0}".format(line.rstrip()))
+    removeHosts(line)
+    addHosts(line)
 
-    try:
-        with open(masterFile) as f1:
-            mhl=f1.readlines()
-    except IOError as e:
-        print("({0})".format(e))
-
-    if bhlExists == True:
-        f = NamedTemporaryFile()
-        for entry in mhl:
-            if re.match("#xCAT#",entry):
-                entry=entry.split()[1]
-                f.write("{0}\n".format(entry))
-        f.flush()
-
-        command="diff --suppress-common-lines -y {0} {1}".format(f.name,buildFile)
-        p1=Popen(command.split(),stdout=PIPE).communicate()[0]
-
-        for entry in p1.split('\n'):
-            if len(entry)>0:
-                buildList.append(entry.split()[0])
-
-            if len(buildList)==0:
-               sys.exit(0)
-            else:
-                for entry in buildList:
-                    fields=entry.split('|')
-                    #if len(chassis)>0 and len(slot)>0:
-                    (ip,mac,host,groups,chassis,slot)=fields[0:6]
-                     #   print(host)
-                    #else:
-                     #   (ip,mac,host,groups)=fields
-                     #   print(host)
-
-                    Popen(['noderm',host],stdout=PIPE,stderr=PIPE).communicate()
-
-                    if len(chassis)>0 and len(slot)>0:
-                        prepareHosts(ip,mac,host,groups,chassis,slot)
-                    else:
-                        prepareHosts(ip,mac,host,groups,"","")
-                    
-
-    elif bhlExists == False:
-        for entry in mhl:
-            if re.match("#xCAT#",entry):
-                mhlList.append(entry)
-
-        Popen(['noderm', 'all'],stdout=None)
-
-        for entry in mhlList:
-            fields=entry.split()[1].split('|')
-            #print(fields)
-            (ip,mac,host,groups,chassis,slot,extra)=fields
-
-            if len(chassis)>0 and len(slot)>0:
-                prepareHosts(ip,mac,host,groups,chassis,slot)
-            else:
-                prepareHosts(ip,mac,host,groups,"","")
-
-
-    print("{0} {1}\n".format(tmpBhl.name,buildFile))
-    try:
-        Popen("mv {0} {1}".format(tmpBhl.name,buildFile),shell=True)
-    except IOError as e:
-        print("({0})".format(e))
-    except OSError as e:
-        print("({0})".format(e))
-
+    
+#Cleanup
+buildFile.close()
+os.remove(buildFileName)
