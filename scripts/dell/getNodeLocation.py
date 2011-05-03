68c213d91390cdad0dc824a6f795a030782b9409
ali <ali@95e82132-2061-11dd-9cc4-33af95911ecc>
2011-05-03 20:16:34 +0000

    Bug fixes
    git-svn-id: https://subversion01.broadinstitute.org/APSG@1896 95e82132-2061-11dd-9cc4-33af95911ecc

--- scripts/dell/getNodeLocation.py
+++ scripts/dell/getNodeLocation.py
@@ -3,13 +3,14 @@ import sys,os
 
 ssh_lib_path = '/broad/tools/scripts'
 ofile_path='/sysman/scratch/cfengine3'
-destination = open('%s/nodepos.csv' % ofile_path, 'w')
+destination = open('%s/nodepostmp.csv' % ofile_path, 'w')
 destination.write("#node,rack,u,chassis,slot,room,comments,disable\n")
 
 sys.path.append(ssh_lib_path)
 import ssh
 
-for cmcNum in range(50,93):
+#for cmcNum in range(50,93):
+for cmcNum in range(83,93):
 	cmc = "brsa%s" % cmcNum
 
 	#try:
@@ -18,16 +19,18 @@ for cmcNum in range(50,93):
 	s.close()
 
 	for item in output:
-		if (
-			'Slot' in item or
-			'SLOT' in item or
-			'node1493' in item
-			): continue
 		fields=item.split()
+		if not 2 <= len(fields) <= 3: continue
 		slot=fields[0]
 		if len(fields)==2: hostname=fields[1]
 		elif len(fields)==3: hostname=fields[2]
-#		print("{0}|{1}|{2:02d}|".format(hostname,cmc,int(slot)))
+		if (
+			'Slot' in hostname or
+			'SLOT' in hostname or
+			'node1493' in hostname
+		   ): continue
+		
+		#print("{0}|{1}|{2:02d}|".format(hostname,cmc,int(slot)))
 		destination.write("{0},,,{1},{2:02d},,\n".format(hostname,cmc,int(slot)))
 		
 	#except Exception:
